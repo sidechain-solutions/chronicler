cff0c0644a345f376e21081f062ca577eb997d90
Davi <davilinfo@gmail.com>
2022-01-02 12:21:42 -0300

    changing expiration time of transaction

--- src/app/helper/api.js
+++ src/app/helper/api.js
@@ -1,4 +1,5 @@
 const { apiClient, codec, cryptography, transactions } = require( '@liskhq/lisk-client');
+const fs = require('fs');
 
 var archiveBinarySchema = {
     $id: 'lisk/archivebinary/transaction',
@@ -79,6 +80,26 @@ class ApiHelper{
         return await client.transaction.get(Buffer.from(transactionId, 'hex'));
     }
 
+    async getTransactionsFromPool(){        
+        const client = await this.getClient();  
+        const schema = await client.invoke('app:getSchema');
+        const transactions = await client.invoke('app:getTransactionsFromPool', {});      
+        
+        var transactionList = [];
+        for (var index=0;index< 1;index++)
+        {                        
+            console.log(transactions.length);
+            
+            console.log(transactions);
+
+            var transaction = transactions[1];
+            var decoded = await codec.codec.decodeJSON(schema.transaction, Buffer.from(transaction, 'hex'));
+            console.log("transaction from pool", decoded);
+            transactionList.push(transaction);
+        };
+        return transactionList;
+    }
+
     async getDisconnectedPeers(){
         const client = await this.getClient();        
         const nodeInfo = await client.invoke('app:getDisconnectedPeers', {});
@@ -100,19 +121,37 @@ class ApiHelper{
         return result;
     }
     
+    async registeredActions(){
+        const client = await this.getClient();        
+        const registeredActions = await client.invoke('app:getRegisteredActions', {});
+
+        return registeredActions;
+    } 
 
     async createArchiveBinaryAssetAndSign(archive, credential){
         const sender = cryptography.getAddressAndPublicKeyFromPassphrase(credential.passphrase);
         
         var accountNonce = await this.getAccountNonce(sender.address);                
         
+        console.log({
+            moduleID: 5000,
+            assetID: 102,
+            nonce: BigInt(accountNonce),
+            fee: BigInt(JSON.stringify(archive).length * 10000 * 10),
+            senderPublicKey: sender.publicKey,
+            asset: {
+                data: JSON.stringify(archive),
+                recipientAddress: sender.address
+            },
+        });
+
         const tx = await transactions.signTransaction(
             archiveBinarySchema,
             {
                 moduleID: 5000,
                 assetID: 102,
                 nonce: BigInt(accountNonce),
-                fee: BigInt(Math.round(JSON.stringify(archive).length * 10000 * 3)),
+                fee: BigInt(Math.round(JSON.stringify(archive).length * 10000 * 5)),
                 senderPublicKey: sender.publicKey,
                 asset: {
                     data: JSON.stringify(archive),
@@ -136,7 +175,7 @@ class ApiHelper{
                 moduleID: 5000,
                 assetID: 101,
                 nonce: BigInt(accountNonce),
-                fee: BigInt(Math.round(JSON.stringify(archive).length * 10000 * 3)),
+                fee: BigInt(JSON.stringify(archive).length * 10000 * 5),
                 senderPublicKey: sender.publicKey,
                 asset: {
                     data: JSON.stringify(archive),
@@ -179,17 +218,25 @@ class ApiHelper{
 
 function initiateTest(){
     var client = new ApiHelper('ws://api.chronicler.cc:8080/ws');
-    
-    client.getAccountFromAddress("608b72e89d6daee341d8926db26936b435cc062d").then(function(data){
+
+    var credential = {passphrase: "trip tank female pulp deposit chunk area shop photo hundred brother treat"};
+
+    client.registeredActions().then(function(data){
+        console.log(data);
+    })
+
+    client.getTransactionsFromPool().then(function(data){
+        console.log(data);
+    });
+
+    /*client.getAccountFromAddress("608b72e89d6daee341d8926db26936b435cc062d").then(function(data){
         console.log(data);
     });        
 
     client.getBlockByHeight(1021).then(function(data){
         console.log(data);
     });
-        
-    var credential = {passphrase: "there roof brown resemble girl soap pass phone bracket pig dust prefer"};
-
+    
     var textArchive = {
         "title": "text archive example 1",        
         "text": "A text example to be verified"
@@ -206,10 +253,10 @@ function initiateTest(){
     }).catch(function(e){
         console.log("Error creating text archive transaction", e);
     });
-
+    
     var binaryArchive = {
         "title": "text binary example 1",        
-        "binary": Buffer.from("A text example to be verified", "utf8")
+        "binary": client.textToBinary("A text example to be verified", "utf8")
     };
     
     console.log("binary archive length", JSON.stringify(binaryArchive).length);
@@ -224,7 +271,28 @@ function initiateTest(){
     }).catch(function(e){
         console.log("Error creating binary archive transaction", e);
     });
+    
 
+    var file = fs.readFileSync('./utxo.jpg', 'binary');
+
+    var binaryFile = {
+        "title": "text binary example 1",        
+        "binary": file
+    };
+
+    console.log("binary archive length", JSON.stringify(binaryFile).length);
+    client.createArchiveBinaryAssetAndSign(binaryFile, credential).then(function(response){
+        console.log("transaction created", response);
+
+        client.sendTransaction(response).then(function(tx){
+            console.log("archive binary transaction sent", tx);
+        }).catch(function(e){
+            console.log("Error sending binary archive transaction", e);
+        });
+    }).catch(function(e){
+        console.log("Error creating binary archive transaction", e);
+    });
+    */
     client.setNewBlockEventSubscriber();
 
     client.setNewTransactionEventSubscriber();
